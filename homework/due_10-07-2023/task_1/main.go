ab6116bab906da99df6823f314d8b00f08d78bf9
yarsult <$@W$@w25-77047>
2023-10-03 21:47:31 +0300

    task1

--- homework/due_10-07-2023/task_1/main.go
+++ homework/due_10-07-2023/task_1/main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+func fibonacci() func(int) int {
+	nums := []int{0, 1, 1}
+	return func(n int) int {
+		if n < 3 {
+			return nums[n]
+		}
+		nums = nums[:n]
+		nums = append(nums, nums[n-1]+nums[n-2])
+		return nums[n]
+	}
+}
+
 func main() {
-	fmt.Println("i don't work yet")
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f(i))
+	}
 }
